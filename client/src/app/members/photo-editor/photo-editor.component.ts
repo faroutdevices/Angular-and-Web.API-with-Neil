71e550372e3c1025f7cca63b7beb7d32cb1fe2a8
uniqueusername10@gmail.com <uniqueusername10@gmail.com>
2020-12-17 17:33:49 -0600

    End of section 11

--- client/src/app/members/photo-editor/photo-editor.component.ts
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -2,8 +2,10 @@ import { Component, OnInit, Input } from '@angular/core';
 import { FileUploader } from 'ng2-file-upload';
 import { take } from 'rxjs/operators';
 import { Member } from 'src/app/_models/member';
+import { Photo } from 'src/app/_models/photo';
 import { User } from 'src/app/_models/user';
 import { AccountService } from 'src/app/_services/account.service';
+import { MembersService } from 'src/app/_services/members.service';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -18,7 +20,7 @@ hasBaseDropZoneOver: false;
 baseUrl = environment.apiUrl;
 user: User;
 
-  constructor(private accountService: AccountService) {
+  constructor(private accountService: AccountService, private memberService: MembersService) {
     this.accountService.currentUser$.pipe(take(1)).subscribe(user => this.user = user);
    }
 
@@ -29,6 +31,24 @@ user: User;
   fileOverBase(e: any) {
     this.hasBaseDropZoneOver = e;
   }
+
+deletePhoto(photoId: number) {
+  this.memberService.deletePhoto(photoId).subscribe(() => {
+    this.member.photos = this.member.photos.filter(x => x.id !== photoId);
+  })
+}
+
+setMainPhoto(photo: Photo) {
+  this.memberService.setMainPhoto(photo.id).subscribe(() => {
+    this.user.photoUrl = photo.url
+    this.accountService.setCurrentUser(this.user);
+    this.member.photoUrl = photo.url;
+    this.member.photos.forEach(p => {
+      if (p.isMain) p.isMain = false;
+      if (p.id === photo.id) p.isMain = true;
+    })
+  })
+}
 
 initializeUploader()
 {
