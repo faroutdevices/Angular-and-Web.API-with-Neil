6e59cf423a74b5072582ee4cb4dc5544ab9ec82a
uniqueusername10@gmail.com <uniqueusername10@gmail.com>
2020-12-18 15:18:48 -0600

    End of section 12

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, Output, OnInit, EventEmitter } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
 import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -10,12 +11,13 @@ import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Vali
 })
 export class RegisterComponent implements OnInit {
 @Output() cancelRegister = new EventEmitter();
-  model: any = {};
   registerForm: FormGroup;
   maxDate: Date;
+  validationErrors: string[] = [];
 
 
-  constructor(private accountService: AccountService, private toastr: ToastrService, private fb: FormBuilder) { }
+  constructor(private accountService: AccountService, private toastr: ToastrService,
+     private fb: FormBuilder, private router: Router) { }
 
   ngOnInit(): void {
     this.initializeForm();
@@ -53,14 +55,11 @@ return (control: AbstractControl) => { return control?.value === control?.parent
 
 
   register() {
-    console.log(this.registerForm.value);
-            // this.accountService.register(this.model).subscribe(response => {
-            //   console.log(response);
-            //   this.cancel();
-            // }, error => {
-            //   console.log("error");
-            //   this.toastr.error(error.error);
-            // })
+    this.accountService.register(this.registerForm.value).subscribe(response => {
+      this.router.navigateByUrl('/members');
+    }, error => {
+      this.validationErrors = error;
+    })
            }
 
   cancel(){
